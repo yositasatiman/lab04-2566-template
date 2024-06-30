5050357a373175f66a4bf4c7de7c9098072f08ad
D. Potikanond <potikanond@gmail.com>
2024-06-30 22:42:09 +0700

    Update and rename q3.js to q3.ts
    change variable names

---
+++ q3.ts
@@ -0,0 +1,13 @@
+function primeNumber(a) {
+  /* Your code here */
+}
+
+const d1 = 10;
+const d2 = 29;
+const d3 = 2;
+
+console.log(primeNumber(d1));
+console.log(primeNumber(d2));
+console.log(primeNumber(d3));
+
+module.exports = primeNumber;
