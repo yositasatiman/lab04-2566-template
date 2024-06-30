6e07d78392722bd1aceb97e145ec8f574075bef9
D. Potikanond <potikanond@gmail.com>
2024-06-30 22:41:22 +0700

    Update and rename q2.js to q2.ts
    change variable names

---
+++ q2.ts
@@ -0,0 +1,13 @@
+function arrayStringify(a) {
+  /* Your code here */
+}
+
+const c1 = [1, 2, 3];
+const c2 = [10, 9, 8, 7, 6, 5];
+const c3 = [];
+
+console.log(arrayStringify(c1));
+console.log(arrayStringify(c2));
+console.log(arrayStringify(c3));
+
+module.exports = arrayStringify;
